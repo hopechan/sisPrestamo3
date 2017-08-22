ac3edf35dd158313b012afa9a929d49b8f9d9696
hopechan <da13002@ues.edu.sv>
2017-08-22 10:40:25 -0600

    falta agregar imagen

--- login.php
+++ login.php
@@ -17,23 +17,29 @@
                 </div>
             </div>
         </nav>
-        <form action="index.php">
-          <div class="imgcontainer">
-            <img src="img_avatar2.png" alt="Avatar" class="avatar">
+        <form class="form-horizontal" method="POST" action="index.php">
+          <fieldset>
+            <h3 align="center" >Bienvenido/a</h3>
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="Usuario">Usuario</label>
+          <div class="col-md-4">
+            <input id="Usuario" name="Usuario" type="text" placeholder="Ingrese nombre de usuario" class="form-control input-md" required="">
           </div>
-
-          <div class="container">
-            <label><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required>
-            <label><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required>
-            <button type="submit">Login</button>
-            <input type="checkbox" checked="checked"> Remember me
+        </div>
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="password">Contraseña</label>
+          <div class="col-md-4">
+            <input id="password" name="password" type="password" placeholder="Ingrese contraseña" class="form-control input-md" required="">
           </div>
-          <div class="container" style="background-color:#f1f1f1">
-            <button type="button" class="cancelbtn">Cancel</button>
-            <span class="psw">Forgot <a href="#">password?</a></span>
+        </div>
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="login"></label>
+            <div class="col-md-8">
+              <button id="login" name="login" class="btn btn-primary"><span class="glyphicon glyphicon-log-in"></span> Entrar</button>
+              <button id="limpiar" name="limpiar" class="btn btn-danger">Cancelar</button>
+            </div>
           </div>
-        </form>
+        </fieldset>
+      </form>
     </body>
     </html>
