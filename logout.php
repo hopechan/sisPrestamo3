6baabfcdf0e384a2ad51f1a96fdffd3743c41cfa
hopechan <da13002@ues.edu.sv>
2017-10-07 16:37:36 -0600

    Cerrar sesion

--- logout.php
+++ logout.php
@@ -1,18 +1,19 @@
 <?php
 require_once 'Bitacora.php';
-session_start();
-session_destroy();
-//Objetos bitacora
-$b = new Bitacora();
-$controladorBitacora = new Bitacora();
-$fecha = date('Y-m-d');
-$hora = date('h:i:s');
-$accion = "El usuario ".$_SESSION["userName"]." cerro sesion ";
-$id_bitacora = $controladorBitacora->maxID($_SESSION["id_usuario"]);
-$b->setId_bitacora($id_bitacora);
-$b->setId_usuario($_SESSION["id_usuario"]);
-$b->setFecha($fecha . " " . $hora);
-$b->setAccion($accion);
-$controladorBitacora->agregar($b);
-header("Location: login.php");
+  //Recupero la sesion actual
+  session_start();
+  //Objetos bitacora
+  $controladorBitacora = new Bitacora();
+  $fecha = date('Y-m-d');
+  $hora = date('h:i:s');
+  $accion = "El usuario ".$_SESSION["userName"]." cerro sesion ";
+  $id_bitacora = $controladorBitacora->maxID($_SESSION["id_usuario"]);
+    $b = new Bitacora();
+    $b->setId_bitacora($id_bitacora);
+    $b->setId_usuario($_SESSION["id_usuario"]);
+    $b->setFecha($fecha . " " . $hora);
+    $b->setAccion($accion);
+    $controladorBitacora->agregar($b);
+    session_destroy();
+    header("Location: login.php");
  ?>
