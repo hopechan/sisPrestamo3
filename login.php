0c13b3c7b254ab1e2b69d20f7a5669edcd4194c3
hopechan <da13002@ues.edu.sv>
2017-11-29 19:41:59 -0600

    Elimine error login

--- login.php
+++ login.php
@@ -15,31 +15,6 @@
     <!-- Custom styles for this template -->
     <link href="css/login.css" rel="stylesheet">
   </head>
-<?php
-require_once 'Bitacora.php';
-  
-  session_start();
-
-  if ($_SESSION["estado"] == "activo") {
-
-      $controladorBitacora = new Bitacora();
-      $id_usuario = $_SESSION["id_usuario"];
-      $fecha = date('Y-m-d');
-      $hora = date('h:i:s');
-      $usuario = $_SESSION["userName"];
-      $accion = "El usuario ".$usuario." cerro sesion ";
-      $id_bitacora = $controladorBitacora->maxID($id_usuario);
-        $b = new Bitacora();
-        $b->setId_bitacora($id_bitacora);
-        $b->setId_usuario($id_usuario);
-        $b->setFecha($fecha . " " . $hora);
-        $b->setAccion($accion);
-        $controladorBitacora->agregar($b);
-        session_destroy();
-  }
-
-
- ?>
   <body>
       <nav class="navbar navbar-inverse navbar-fixed-top">
           <div class="container-fluid">
